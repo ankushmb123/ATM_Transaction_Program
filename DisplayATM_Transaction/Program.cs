a67acc3f7d1972e9100deb3d8f8789bcb729a9e8
Ankush Bahekar <ankushmb9@gmail.com>
2023-07-11 22:13:20 +0530

    [Ankush] Add:Performing Transaction Operations.

---
+++ DisplayATM_Transaction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace DisplayATM_Transaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayATM displayATM = new DisplayATM();
+            displayATM.Display();
+
+        }
+
+    }
+}
